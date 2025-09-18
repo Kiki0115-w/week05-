b3ece52968df7cda0243ca32eafeb909383ed865
Kiki0115-w <kanyapak.wongt@bumail.net>
2025-09-18 10:23:06 +0700

    up
    ช้อย 3 ข้อ

--- Assets/Scripts/Workspace/Assignment/StudentSolution.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution.cs
@@ -13,17 +13,78 @@ namespace Assignment
         #region Lecture
         public void LCT01_SelectionSortAscending(int[] numbers)
         {
-            throw new System.NotImplementedException();
+            int n = numbers.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (numbers[j] < numbers[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                int temp = numbers[minIndex];
+                numbers[minIndex] = numbers[i];
+                numbers[i] = temp;
+            }
+
+            foreach (int n1 in numbers)
+            {
+                Debug.Log(n1);
+            }
         }
 
         public void LCT02_BubbleSortAscending(int[] numbers)
         {
-            throw new System.NotImplementedException();
+            int n = numbers.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j]; // [..., 4, 2, ...] temp = 4
+                        numbers[j] = numbers[j + 1]; // [..., 2, 2, ...] = temp = 4
+                        numbers[j + 1] = temp; // [..., 2, 4, ...]
+                    }
+                }
+            }
+
+            foreach ( int n1 in numbers) 
+            {
+                Debug.Log(n1);
+            }
         }
 
         public void LCT03_InsertionSortAscending(int[] numbers)
         {
-            throw new System.NotImplementedException();
+            int n = numbers.Length;
+            for (int i = 0; i < n; i++)
+            {
+                int key = numbers[i];
+                int j = i - 1;
+                while (j >= 0)
+                {
+                    if (key > numbers[j])
+                    {
+                        break;
+                    } // หยุดเลื่อน
+
+                    // เลื่อนไปทางขวา
+                    numbers[j + 1] = numbers[j];
+                    j--;
+                }
+
+                numbers[j + 1] = key; // แทรก
+            }
+
+            foreach ( int n1 in numbers)
+            {
+                Debug.Log(n1);
+            }
+
         }
 
         #endregion
